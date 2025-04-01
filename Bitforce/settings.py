c1ebe162409ab91ec8016f51270f5093a82056be
vani <va.fuentes@alumno.um.edu.ar>
2025-04-01 20:53:47 -0300

    DB conection

--- Bitforce/settings.py
+++ Bitforce/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'Bitforce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bitforce',  
+        'USER': 'root',         
+        'PASSWORD': '1234',  
+        'HOST': 'localhost',          
+        'PORT': '3306',               
+        'OPTIONS': {
+            'charset': 'utf8mb4',  
+        }
     }
 }
+
 
 
 # Password validation
@@ -122,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- BitforceApp/admin.py
+++ BitforceApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import GymUser, Sucursal, Coach, Actividad, Turno
+
+admin.site.register(Sucursal)
 
 # Register your models here.
