c29aacce065b25219e0dbb5e95933e52f0e42758
vani <va.fuentes@alumno.um.edu.ar>
2025-04-03 21:16:19 -0300

    Prueba

--- BitforceApp/views.py
+++ BitforceApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializer import *
+from .models import *
+
+class GymUserView(viewsets.ModelViewSet):
+    serializer_class = GymUserSerializer
+    queryset = GymUser.objects.all()
+
+class SucursalView(viewsets.ModelViewSet):
+    serializer_class = SucursalSerializer
+    queryset = Sucursal.objects.all()
+
+class CoachView(viewsets.ModelViewSet):
+    serializer_class = CoachSerializer
+    queryset = Coach.objects.all()
+
+class ActividadView(viewsets.ModelViewSet):
+    serializer_class = ActividadSerializer
+    queryset = Actividad.objects.all()
+
+class TurnoView(viewsets.ModelViewSet):
+    serializer_class = TurnoSerializer
+    queryset = Turno.objects.all()
+
+
+
